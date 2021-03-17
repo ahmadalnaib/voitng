3b5c7dead837e96f7db6091cd73f2d18234ebe4d
ahmedhabeeb73 <alnaib888@gmail.com>
2021-03-18 00:41:13 +0100

    setup and design

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,76 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="csrf-token" content="{{ csrf_token() }}">
+
+        <title>{{ config('app.name', 'Dundung') }}</title>
+
+        <!-- Fonts -->
+        <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap">
+
+        <!-- Styles -->
+        <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+
+        <!-- Scripts -->
+        <script src="{{ asset('js/app.js') }}" defer></script>
+    </head>
+    <body class="font-sans bg-gray-background text-gray-900 text-sm">
+        <header class="flex items-center justify-between px-8 py-4">
+            <a href="{{route('home')}}"><img class="w-9/12" src="{{asset('img/default.svg')}}" alt="logo"></a>
+            <div class="flex items-center">
+                @if (Route::has('login'))
+                    <div class="px-6 py-4 ">
+                        @auth
+                            <form method="POST" action="{{ route('logout') }}">
+                                @csrf
+
+                                <a href="{{route('logout')}}"
+                                                 onclick="event.preventDefault();
+                                                this.closest('form').submit();">
+                                    {{ __('Log out') }}
+                                </a>
+                            </form>
+                        @else
+                            <a href="{{ route('login') }}" class="text-sm text-gray-700 underline">Log in</a>
+
+                            @if (Route::has('register'))
+                                <a href="{{ route('register') }}" class="ml-4 text-sm text-gray-700 underline">Register</a>
+                            @endif
+                        @endauth
+                    </div>
+                @endif
+                <a href="#">
+                    <img class="w-10 h-10 rounded-full" src="" alt="">
+                </a>
+            </div>
+        </header>
+
+{{--    showcase--}}
+
+    <main class="container mx-auto max-w-custom flex">
+        <div class="w-70 mr-5">
+           Add job here
+        </div>
+        <div class="w-175">
+    <nav class="flex items-center justify-between text-xs">
+        <ul class="flex uppercase font-semibold space-x-10 border-b-4 pb-3">
+            <li><a class="border-b-4 pb-3 border-blue-600" href="">All Jobs (80)</a></li>
+            <li><a class="text-gray-400 transition duration-150 ease-in border-b-4 pb-3 hover:border-blue-500" href="#">Considering (40)</a></li>
+            <li><a class="text-gray-400 transition duration-150 ease-in border-b-4 pb-3 hover:border-blue-500" href="#">In Progress (10)</a></li>
+        </ul>
+        <ul class="flex uppercase font-semibold border-b-4 pb-3 space-x-10">
+            <li><a class="text-gray-400 transition duration-150 ease-in border-b-4 pb-3 hover:border-blue-500" href="#">Implemented (10)</a></li>
+            <li><a class="text-gray-400 transition duration-150 ease-in border-b-4 pb-3 hover:border-blue-500" href="#">Closed (10)</a></li>
+
+        </ul>
+    </nav>
+            <div class="mt-8">
+                {{$slot}}
+            </div>
+        </div>
+
+    </main>
+    </body>
+</html>
---
+++ tailwind.config.js
@@ -0,0 +1,36 @@
+const defaultTheme = require('tailwindcss/defaultTheme');
+
+module.exports = {
+    purge: [
+        './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
+        './storage/framework/views/*.php',
+        './resources/views/**/*.blade.php',
+    ],
+
+    theme: {
+        extend: {
+            colors:{
+                'gray-background':'#f7f8fc',
+            },
+            maxWidth:{
+             custom:"62.5rem",
+            },
+            spacing:{
+              70:'17.5rem',
+               175:'43.75rem'
+            },
+
+            fontFamily: {
+                sans: ['Nunito', ...defaultTheme.fontFamily.sans],
+            },
+        },
+    },
+
+    variants: {
+        extend: {
+            opacity: ['disabled'],
+        },
+    },
+
+    plugins: [require('@tailwindcss/forms')],
+};
